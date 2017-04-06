1fcd798fd7c9e07d2d4132d2ac4d5959b7666e8c
Ayke van Laethem <aykevanlaethem@gmail.com>
2017-04-06 22:52:04 +0200

    Auto-expand

--- skins/base/common.js
+++ skins/base/common.js
@@ -11,6 +11,30 @@ function setCookie (key, value, maxAge, path, domain, secure) {
 	document.cookie = encodeURIComponent(key) + "=" + encodeURIComponent(value) + (maxAge ? "; max-age=" + maxAge : "") + (domain ? "; domain=" + domain : "") + (path ? "; path=" + path : "") + (secure ? "; secure" : "");
 }
 
+// https://jsbin.com/qizepev/edit?html,css,js,output
+function enableAutoExpand() {
+
+	let textareas = document.querySelectorAll('.autoexpand');
+	for (let i=0; i<textareas.length; i++) {
+		textareas[i].classList.add('autoexpand-enabled');
+
+		let hiddenDiv = document.createElement('div');
+		hiddenDiv.classList.add('autoexpand-hidden');
+		textareas[i].parentNode.insertBefore(hiddenDiv, textareas[i]);
+
+		let cs = getComputedStyle(textareas[i]);
+		let textareaMargin = parseFloat(cs.borderLeftWidth) + parseFloat(cs.paddingLeft) + parseFloat(cs.paddingRight) + parseFloat(cs.borderRightWidth);
+
+		textareas[i].addEventListener('input', function () {
+			let content = this.value;
+			hiddenDiv.style.width = (this.getBoundingClientRect().width - textareaMargin).toFixed(3) + 'px';
+			hiddenDiv.textContent = content + '\n\n';
+			this.style.height = hiddenDiv.getBoundingClientRect().height + 'px';
+		});
+		textareas[i].dispatchEvent(new Event('input'));
+	}
+}
+
 function onLoad() {
 	var forms = document.querySelectorAll('form[method=POST]');
 	if (forms.length == 0) {
@@ -37,6 +61,8 @@ function onLoad() {
 		input.value = csrftoken;
 		form.appendChild(input);
 	}
+
+	enableAutoExpand();
 }
 
 if (document.readyState == 'interactive' || document.readyState == 'complete') {
